c8a17c758d6f5f76388ef4063733815ed6555ab0
Kaushik <skaushik@mcdermott.com>
2019-09-04 17:28:29 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Discipline {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TechBidEvaluation';
+
+  disciplines: Discipline[] = [
+    { id: 1, name: 'Process' },
+    { id: 2, name: 'Piping' },
+    { id: 3, name: 'Instrumentation' },
+    { id: 4, name: 'Electrical' },
+    { id: 5, name: 'Structural' },
+    { id: 6, name: 'Pipelines' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CardComponent } from './card.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule, MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CardComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
